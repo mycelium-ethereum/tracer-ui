cc022ec44c277fbca5181d7db601a37d75cd110c
Shawn Doyle <shawndoyle@Shawns-MacBook-Pro.local>
2021-12-22 13:59:27 -0500

    chore: add themes to storybook UI

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
 import { ThemeProvider } from "../src/theme";
 import { useDarkMode } from "storybook-dark-mode";
+import { themes } from "@storybook/theming";
+import { lightTheme, darkTheme } from "../src/theme";
+import "./global.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "centered",
+  darkMode: {
+    dark: {
+      ...themes.dark,
+      appBg: darkTheme.colors.cell.active,
+      appContentBg: darkTheme.colors.cell.background,
+      barBg: darkTheme.colors.cell.background,
+    },
+    light: {
+      ...themes.normal,
+      appBg: lightTheme.colors.cell.active,
+      appContentBg: lightTheme.colors.cell.background,
+      barBg: lightTheme.colors.cell.background,
+    },
+  },
 };
 
 export const decorators = [
