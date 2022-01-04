c47d720059c1d8e87a20ad1fbbe74d0b84eb3ad7
Shawn Doyle <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-01-04 17:03:08 -0500

    lint: remove unused test code

--- src/atoms/Card/Card.test.tsx
+++ src/atoms/Card/Card.test.tsx
@@ -1,24 +1,19 @@
 // Generated with util/create-component.js
-import React from "react";
-import { render } from "@testing-library/react";
+// import React from "react";
+// import { render } from "@testing-library/react";
 
-import Card from "./Card";
-import { CardProps } from "./Card.types";
+// import Card from "./Card";
+// import { CardProps } from "./Card.types";
 
 describe("Test Component", () => {
-  let props: CardProps;
-
-  beforeEach(() => {
-    props = {};
-  });
-
-  const renderComponent = () => render(<Card {...props} />);
-
-  // it("should render foo text correctly", () => {
-  //   const { getByTestId } = renderComponent();
-
-  //   const component = getByTestId("Card");
-
-  //   expect(component).toHaveTextContent("harvey was here");
-  // });
+    // let props: CardProps;
+    // beforeEach(() => {
+    //   props = {};
+    // });
+    // const renderComponent = () => render(<Card {...props} />);
+    // it("should render foo text correctly", () => {
+    //   const { getByTestId } = renderComponent();
+    //   const component = getByTestId("Card");
+    //   expect(component).toHaveTextContent("harvey was here");
+    // });
 });
