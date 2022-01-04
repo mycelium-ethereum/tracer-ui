beca11d2206db7ba2256d10cabd631034c7feed4
Shawn Doyle <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-01-04 12:14:49 -0500

    feat: create slider component

---
+++ src/atoms/Slider/Slider.tsx
@@ -0,0 +1,68 @@
+// Generated with util/create-component.js
+import React from "react";
+import styled from "styled-components";
+
+import { SliderProps } from "./Slider.types";
+
+const Slider: React.FC<SliderProps> = ({ value, min, max, step, onChange }) => (
+    <div data-testid="slider">
+        <StyledSlider
+            type="range"
+            value={value}
+            min={min}
+            max={max}
+            step={step}
+            onChange={(ev) => onChange(Number(ev.target.value))}
+        />
+    </div>
+);
+
+export default Slider;
+
+const StyledSlider = styled.input`
+    background: ${(props) => {
+        const { value, min, max } = props;
+        const percent =
+            ((Number(value) - Number(min)) / (Number(max) - Number(min))) * 100;
+        return `linear-gradient(
+            to right, 
+            ${props.theme.colors.action.inactive} 0%, 
+            ${props.theme.colors.action.inactive} ${percent}%, 
+            ${props.theme.colors.cell.highlight} ${percent}%, 
+            ${props.theme.colors.cell.highlight} 100%
+        )`;
+    }};
+    border-radius: 8px;
+    height: 20px;
+    width: 100%;
+    outline: none;
+    transition: background 450ms ease-in;
+    -webkit-appearance: none;
+
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        background: ${(props) => props.theme.colors.action.active};
+        border: none;
+        border-radius: 50%;
+        height: 10px;
+        width: 10px;
+        transform: scale(3);
+    }
+    &::-moz-range-thumb {
+        background: ${(props) => props.theme.colors.action.active};
+        border: none;
+        border-radius: 50%;
+        height: 10px;
+        width: 10px;
+        transform: scale(3);
+    }
+
+    &:hover::-webkit-slider-thumb {
+        transition: transform 0.1s ease-in;
+        transform: scale(3.5);
+    }
+    &:hover::-moz-range-thumb {
+        transition: transform 0.1s ease-in;
+        transform: scale(3.5);
+    }
+`;
---
+++ src/atoms/Slider/Slider.types.ts
@@ -0,0 +1,8 @@
+// Generated with util/create-component.js
+export interface SliderProps {
+    value: number;
+    min: number;
+    max: number;
+    step?: number;
+    onChange?: (value: number) => void;
+}
