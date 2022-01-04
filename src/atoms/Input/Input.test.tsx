d43ee7594329c07a5b17f4c304af5559ea247b85
Shawn Doyle <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-01-04 09:46:15 -0500

    chore: remove unused vars

--- src/atoms/Input/Input.test.tsx
+++ src/atoms/Input/Input.test.tsx
@@ -1,9 +1,9 @@
 // Generated with util/create-component.js
-import React from "react";
-import { render } from "@testing-library/react";
+// import React from "react";
+// import { render } from "@testing-library/react";
 
-import Input from "./Input";
-import { InputProps } from "./Input.types";
+// import Input from "./Input";
+// import { InputProps } from "./Input.types";
 
 describe("Test Component", () => {
     // let props: InputProps;
--- src/atoms/Textarea/Textarea.test.tsx
+++ src/atoms/Textarea/Textarea.test.tsx
@@ -1,9 +1,9 @@
 // Generated with util/create-component.js
-import React from "react";
-import { render } from "@testing-library/react";
+// import React from "react";
+// import { render } from "@testing-library/react";
 
-import Textarea from "./Textarea";
-import { TextareaProps } from "./Textarea.types";
+// import Textarea from "./Textarea";
+// import { TextareaProps } from "./Textarea.types";
 
 describe("Test Component", () => {
     // let props: TextareaProps;
