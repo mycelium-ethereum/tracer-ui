c65085363b9eccc4b0c1910906ee050e36041c63
Shawn Doyle <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-01-04 16:56:42 -0500

    feat(card): made component less opinionated

--- src/atoms/Card/Card.tsx
+++ src/atoms/Card/Card.tsx
@@ -1,34 +1,48 @@
 // Generated with util/create-component.js
 import React from "react";
 import styled from "styled-components";
-import { device } from "../../helpers";
 
 import { CardProps } from "./Card.types";
 
-const Card: React.FC<CardProps> = ({ children, maxWidth }) => (
-  <StyledCard data-testid="card" maxWidth={maxWidth}>
-    {children}
-  </StyledCard>
+const Card: React.FC<CardProps> = ({ children, ...props }) => (
+    <StyledCard data-testid="card" {...props}>
+        {children}
+    </StyledCard>
 );
 
 export default Card;
 
 const StyledCard = styled.div.attrs<CardProps>((props) => ({
-  maxWidth: props.maxWidth,
+    maxWidth: props.maxWidth,
+    color: props.color || "primary",
+    fluid: props.fluid || false,
+    square: props.square || false,
+    shadow: props.shadow || false,
+    padding: props.padding || "md",
 }))<CardProps>`
-  background-color: ${(props) => props.theme.colors.cell.active};
-  color: ${(props) => props.theme.colors.text.active};
-  box-sizing: border-box;
-  padding: 3rem 1rem;
-  border-radius: 0;
-  width: 100%;
-
-  @media ${device.mobileL} {
-    ${(props) => (props.maxWidth ? `max-width: ${props.maxWidth}` : "")};
-    width: calc(100% - 2rem);
-    border-radius: 1.5rem;
+    background-color: ${(props) =>
+        props.color === "highlight"
+            ? props.theme.colors.cell.highlight
+            : props.theme.colors.cell.active};
+    color: ${(props) => props.theme.colors.text.active};
+    box-sizing: border-box;
+    padding: 1rem;
     margin: 0 auto;
-    padding: 4rem;
-    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
-  }
+    width: ${(props) => (props.fluid ? "100%" : "calc(100% - 2rem)")};
+    max-width: ${(props) => props.maxWidth};
+    border-radius: ${(props) => (props.square ? "0" : "1.5rem")};
+    box-shadow: ${(props) =>
+        props.shadow ? "0 0.5rem 1rem rgba(0, 0, 0, 0.1)" : "none"};
+    padding: ${(props) => {
+        switch (props.padding) {
+            case "sm":
+                return "1rem";
+            case "md":
+                return "2rem";
+            case "lg":
+                return "3rem";
+            default:
+                return "1rem";
+        }
+    }};
 `;
--- src/atoms/Card/Card.types.ts
+++ src/atoms/Card/Card.types.ts
@@ -1,4 +1,9 @@
 // Generated with util/create-component.js
 export interface CardProps {
-  maxWidth?: string;
+    maxWidth?: string;
+    fluid?: boolean;
+    square?: boolean;
+    color?: "active" | "highlight";
+    shadow?: boolean;
+    padding?: "sm" | "md" | "lg";
 }
