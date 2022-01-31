070d878892ab9e3acdf1e0ae0bc3be182305674a
doylio <34788350+doylio@users.noreply.github.com>
2022-01-31 11:55:45 -0500

    fix(Navbar): fix export names

--- src/organisms/Navbar/index.ts
+++ src/organisms/Navbar/index.ts
@@ -1,4 +1,4 @@
 // Generated with util/create-component.js
 export { NavbarProps } from "./Navbar.types";
-import { default as Navbar } from "./Navbar";
+export { default as Navbar } from "./Navbar";
 export { default as HeaderSiteSwitcher } from "./HeaderSiteSwitcher";
