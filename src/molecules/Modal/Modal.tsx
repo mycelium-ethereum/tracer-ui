25380ea2f5a2d170faa65aa5551bf91445aea280
doylio <34788350+doylio@users.noreply.github.com>
2022-04-01 11:32:37 -0400

    feat(Modal): create component

---
+++ src/molecules/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+// Generated with util/create-component.js
+import React from "react";
+import ReactModal from "react-modal";
+import { useTheme } from "styled-components";
+
+import { ModalProps } from "./Modal.types";
+
+const Modal: React.FC<ModalProps> = ({
+    open,
+    color = "primary",
+    onOverlayClick,
+    children,
+}) => {
+    const theme = useTheme();
+
+    const backgroundColor = theme.colors.cell[color];
+    return (
+        <ReactModal
+            isOpen={open}
+            style={{
+                overlay: {
+                    backgroundColor: "rgba(255, 255, 255, 0.5)",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                content: {
+                    position: "relative",
+                    maxWidth: "80%",
+                    border: "none",
+                    borderRadius: "16px",
+                    boxShadow:
+                        "0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)",
+                    backgroundColor,
+                },
+            }}
+            onRequestClose={onOverlayClick}
+        >
+            {children}
+        </ReactModal>
+    );
+};
+
+export default Modal;
---
+++ src/molecules/Modal/index.ts
@@ -0,0 +1,3 @@
+// Generated with util/create-component.js
+export { ModalProps } from "./Modal.types";
+export { default as Modal } from "./Modal";
