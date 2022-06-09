2475955a002de09bba0663f0f9ce286023238ab9
doylio <34788350+doylio@users.noreply.github.com>
2022-06-09 10:54:54 -0400

    Remove dead import

--- src/organisms/Navbar/index.ts
+++ src/organisms/Navbar/index.ts
@@ -1,4 +1,3 @@
 // Generated with util/create-component.js
 export { NavbarProps } from "./Navbar.types";
 export { default as Navbar } from "./Navbar";
-export { default as HeaderSiteSwitcher } from "./HeaderSiteSwitcher";
